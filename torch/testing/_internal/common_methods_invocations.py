a703e76b8b729a4d7a9f6d0ab19221b0ea7f47bd
Sun, Jiayi <jiayi.sun@intel.com>
2024-11-19 13:27:03 +0000

    fix sigmoid for torch.complex datatypes on CPU
    ghstack-source-id: 925bd62c6ec8b7e14a8b18aa18b9ee882e0115c4
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/140391

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -20071,9 +20071,9 @@ op_db: List[OpInfo] = [
                    skips=(
                        # Reference: https://github.com/pytorch/pytorch/issues/56012
                        DecorateInfo(unittest.skip("Skipped!"), 'TestUnaryUfuncs', 'test_reference_numerics_extremal',
-                                    dtypes=[torch.complex64, torch.cdouble]),
+                                    dtypes=[torch.complex64, torch.cdouble], device_type='cuda'),
                        DecorateInfo(unittest.skip("Skipped!"), 'TestUnaryUfuncs', 'test_reference_numerics_large',
-                                    dtypes=[torch.chalf, torch.complex64, torch.cdouble])),
+                                    dtypes=[torch.chalf, torch.complex64, torch.cdouble], device_type='cuda')),
                    dtypes=all_types_and_complex_and(torch.bool, torch.float16, torch.bfloat16),
                    dtypesIfCUDA=all_types_and_complex_and(torch.complex32, torch.bool, torch.half, torch.bfloat16),
                    supports_forward_ad=True,
@@ -22565,8 +22565,6 @@ python_ref_db = [
         "_refs.sigmoid",
         torch_opinfo_name="sigmoid",
         aliases=('_refs.special.expit',),
-        # Reference: https://github.com/pytorch/pytorch/issues/56012
-        handles_complex_extremal_values=False,
         handles_large_floats=False,
         decorators=(precisionOverride({torch.float16: 1e-2,
                                        torch.complex64: 1e-1,
@@ -22575,10 +22573,10 @@ python_ref_db = [
             # Reference: https://github.com/pytorch/pytorch/issues/56012
             DecorateInfo(unittest.skip("Skipped!"), 'TestUnaryUfuncs',
                          'test_reference_numerics_extremal',
-                         dtypes=[torch.complex64, torch.cdouble]),
+                         dtypes=[torch.complex64, torch.cdouble], device_type='cuda'),
             DecorateInfo(unittest.skip("Skipped!"), 'TestUnaryUfuncs',
                          'test_reference_numerics_large',
-                         dtypes=[torch.chalf, torch.complex64, torch.cdouble])
+                         dtypes=[torch.chalf, torch.complex64, torch.cdouble], device_type='cuda')
         ),
     ),
     ElementwiseUnaryPythonRefInfo(
